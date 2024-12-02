a492db01c8a896123653fdb910a7606330e58f91
James <150140966+james-uea@users.noreply.github.com>
2024-12-02 16:02:10 +0000

    Upload part 1 of day 2 in Python

---
+++ day-2/part1.py
@@ -0,0 +1,47 @@
+def check_data_safety(data):
+    # Convert all elements to integers
+    tmp_data = []
+    for element in data:
+        tmp_data.append(int(element))
+    data = tmp_data
+
+    increasing = False # Decreasing by default
+
+    # Check the first 2 elements to check if we're increasing or decreasing
+    if data[0] < data[1]:
+        increasing = True
+    elif data[0] == data[1]:
+        # We haven't increased or decreased, this is an unsafe entry
+        return False
+
+    for i in range(len(data) - 1):
+        if increasing:
+            if not data[i] < data[i + 1]:
+                return False
+            elif data[i + 1] - data[i] > 3:
+                return False
+        else:
+            if not data[i] > data[i + 1]:
+                return False
+            elif data[i] - data[i + 1] > 3:
+                return False
+
+    return True
+
+def main():
+    with open("input", "r") as f:
+        lines = f.readlines()
+        num_safe_entries = 0
+
+        for line in lines:
+            line = line.strip()
+            data = line.split()
+
+            if check_data_safety(data): num_safe_entries += 1
+
+        print(f"Total amount of safe entries: {num_safe_entries}")
+
+
+
+if __name__ == "__main__":
+    main()
