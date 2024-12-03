83ba2ac8d700d30ccfae7a97227df9ce40979a15
James <150140966+james-uea@users.noreply.github.com>
2024-12-03 23:22:32 +0000

    Upload day-3 part 1 solution in python

---
+++ day-3/part1.py
@@ -0,0 +1,22 @@
+import re
+
+global output
+output = 0
+
+def mul(x, y):
+    global output
+    output += x * y
+
+def main():
+    with open("input", "r") as f:
+        lines = f.read()
+        pattern = r'mul\(\d{1,3},\d{1,3}\)'
+        results = re.findall(pattern, lines)
+
+        for result in results:
+            exec(result)
+
+        print(output)
+
+if __name__ == "__main__":
+    main()
